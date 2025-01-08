40e5bd8269a3990e980f7243a99f6fd545b370a9
hwan2ya1004 <71603769+hwan2ya1004@users.noreply.github.com>
2025-01-09 00:50:22 +0900

    Create fetchMarketAll.js

---
+++ netlify/functions/fetchMarketAll.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  try {
+    const response = await fetch('https://api.upbit.com/v1/market/all?isDetails=true');
+    
+    if (!response.ok) {
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ error: 'Failed to fetch market data' }),
+      };
+    }
+    
+    const data = await response.json();
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // 모든 도메인에서 접근 허용
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};

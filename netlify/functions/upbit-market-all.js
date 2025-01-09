ab197051449a538fc4f23c3477fc64f0cf177995
hwan2ya1004 <71603769+hwan2ya1004@users.noreply.github.com>
2025-01-09 23:50:24 +0900

    Create upbit-market-all.js

---
+++ netlify/functions/upbit-market-all.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event, context) => {
+  const { isDetails } = event.queryStringParameters || {};
+  const upbitURL = `https://api.upbit.com/v1/market/all?isDetails=${isDetails || 'false'}`;
+
+  try {
+    const response = await fetch(upbitURL);
+    if (!response.ok) {
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ error: 'Error fetching data from Upbit API.' }),
+      };
+    }
+
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error.' }),
+    };
+  }
+};
